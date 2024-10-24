1ba8bcbfdb2ced1835f0af7c0471fcfc6c466fe0
trevor-duane@github.com <trevorkayiira@gmail.com>
2024-10-24 11:49:33 +0300

    second commit

--- app/api/send-mail.js
+++ app/api/send-mail.js
@@ -16,7 +16,7 @@ const cors = initMiddleware(
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
